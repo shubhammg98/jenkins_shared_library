505892cc0c3eb8c4f67ab2c78d9d177c50158348
shubhammg98 <[shubhammg510@gmail.com])>
2023-10-18 21:55:13 +0530

    git commit

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,7 +1,17 @@
 def call(Map stageParams) {
+
     checkout([
         $class: 'GitSCM',
-        branches: [[name:stageParams.branch ]],
-        userRemoteConfigs: [[url:stageParams..url]]
+        branches: [[name: stageParams.branch ]],
+        userRemoteConfigs: [[ url:stageParams..url ]]
     ])
 }
+
+//def call(Map stageParams) {
+ 
+  //  checkout([
+    //    $class: 'GitSCM',
+      //  branches: [[name:  stageParams.branch ]],
+        //userRemoteConfigs: [[ url: stageParams.url ]]
+    //])
+  //}
