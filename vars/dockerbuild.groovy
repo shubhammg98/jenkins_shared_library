9bf9b9ca4235e1ddf8a37d49fd1c5fb3111f9903
shubhammg98 <[shubhammg510@gmail.com])>
2023-10-29 02:14:28 +0530

    checkout

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -2,7 +2,7 @@ def call(String project, String ImageTag, String hubuser){
     
     sh """
         docker image build -t ${hubuser}/${project} .
-        docker image tag ${hubuser)/$(project} ${shubhamdevops}/${project}:${ImageTag}
+        docker image tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
         docker image tag ${hubuser}/${project} ${shubhamdevops}/${project}:Latest
     """
 }
