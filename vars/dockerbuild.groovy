5acb0ebc66b5b04067f8102308590ac79a1916af
shubhammg98 <[shubhammg510@gmail.com])>
2023-11-01 01:54:02 +0530

    git checkout

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -1,9 +1,9 @@
 def call(String project, String ImageTag, String shubhamdevops){
     
-   sudo sh """
-            docker image build -t ${shubhamdevops}/${project} .
-            docker image tag ${shubhamdevops}/${project} ${shubhamdevops}/${project}:${ImageTag}
-            docker image tag ${shubhamdevops}/${project} ${shubhamdevops}/${project}:Latest
-        """
+   sh """
+        docker image build -t ${shubhamdevops}/${project} .
+        docker image tag ${shubhamdevops}/${project} ${shubhamdevops}/${project}:${ImageTag}
+        docker image tag ${shubhamdevops}/${project} ${shubhamdevops}/${project}:Latest
+    """
 
 }
