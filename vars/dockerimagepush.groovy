7e792de42f5d5e42563ef83a6b28cae751d4a155
shubhammg98 <[shubhammg510@gmail.com])>
2023-11-07 00:13:45 +0530

    checkout

--- vars/dockerimagepush.groovy
+++ vars/dockerimagepush.groovy
@@ -23,7 +23,7 @@ def call(String aws_account_id, String region, String ecr_reponame){
         export AWS_ACCESS_KEY_ID=AKIA243XJVHZEYFYSO62
         export AWS_SECRET_ACCESS_KEY=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
         export AWS_DEFAULT_REGION=ap-south-1
-        sudo aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
+         aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
         docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_reponame}:latest
     
     """
