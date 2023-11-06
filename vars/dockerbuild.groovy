268f0b317a680fbc50db686a218b2b75ab9a777e
shubhammg98 <[shubhammg510@gmail.com])>
2023-11-06 23:11:00 +0530

    checkout

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -1,9 +1,17 @@
-def call(String project, String ImageTag, String shubhamdevops){
+//def call(String aws_account_id, String region, String hubUser){
+    
+  // sh """
+    //    docker image build -t ${hubUser} .
+      //  docker image tag ${shubhamdevops}/${project} ${shubhamdevops}/${project}:${ImageTag}
+        //docker image tag ${shubhamdevops}/${project} ${shubhamdevops}/${project}:Latest
+    //"""
+
+//}
+def call(String aws_account_id, String region, String ecr_reponame){
     
    sh """
-        docker image build -t ${shubhamdevops}/${project} .
-        docker image tag ${shubhamdevops}/${project} ${shubhamdevops}/${project}:${ImageTag}
-        docker image tag ${shubhamdevops}/${project} ${shubhamdevops}/${project}:Latest
+        docker image build -t ${ecr_reponame} .
+        docker tag ${ecr_reponame}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_reponame}:latest
     """
 
 }
--- vars/dockerimagepush.groovy
+++ vars/dockerimagepush.groovy
@@ -1,19 +1,31 @@
- def call(String project, String ImageTag, String shubhamdevops){
-     withCredentials([usernamePassword(
-             credentialsId: 'shubhamdevops',
-             passwordVariable: 'PASS',
-             usernameVariable: 'USER'
+ //def call(String project, String ImageTag, String shubhamdevops){
+   //  withCredentials([usernamePassword(
+     //        credentialsId: 'shubhamdevops',
+       //      passwordVariable: 'PASS',
+         //    usernameVariable: 'USER'
 
              
-     )]) {
-         sh "docker login -u '$USER' -p '$PASS'"
-     }
-     sh "docker image push ${shubhamdevops}/${project}:${ImageTag}"
-     sh "docker image push ${shubhamdevops}/${project}:latest"   
- }
+     //)]) {
+       //  sh "docker login -u '$USER' -p '$PASS'"
+     //}
+     //sh "docker image push ${shubhamdevops}/${project}:${ImageTag}"
+     //sh "docker image push ${shubhamdevops}/${project}:latest"   
+ //}
 //withCredentials([usernamePassword(
   //  credentialsId: 'shubhamdevops',
     // passwordVariable: 'PASS',
       //usernameVariable: 'USER')]) {
     // some block
 //}
+def call(String aws_account_id, String region, String ecr_reponame){
+    
+   sh """
+        aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
+        docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_reponame}:latest
+    
+    """
+
+}
+  
+ 
+ 
