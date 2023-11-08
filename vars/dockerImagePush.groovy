e99e1e3e675c3e6e9e9be80fae04fbcb8b8c87bb
shubhammg98 <[shubhammg510@gmail.com])>
2023-11-08 16:09:08 +0530

    checkout

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -17,18 +17,19 @@
       //usernameVariable: 'USER')]) {
     // some block
 //}
-def call(String aws_account_id, String region, String ecr_reponame){
+def call(String aws_account_id, String region, String ecr_repoName){
     
    sh """
         aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
         aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
         aws configure set default.region $region
-        aws ecr get-login-password --region ${region} |  docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
-        docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_reponame}:latest
-    
+        aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
+        docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
     """
-
 }
-  
+    
+
+
+
  
  
