797dba16e57ba1e0d0e89f230dfd1e8ebf78d59a
shubhammg98 <[shubhammg510@gmail.com])>
2023-11-08 16:54:12 +0530

    checkout

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -27,16 +27,16 @@
         //docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
     //"""
 //}
-def call(String aws_account_id, String region, String ecr_repoName){
+//def call(String aws_account_id, String region, String ecr_repoName){
     
-    sh """
-     aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
-     aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
-     aws configure set default.region $region
-     aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
-     docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
-    """
-}
+  //  sh """
+    // aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+     //aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+     //aws configure set default.region $region
+     //aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
+     //docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
+    //"""
+//}
 
 
 
