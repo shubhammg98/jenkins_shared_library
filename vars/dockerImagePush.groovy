c35db846fa8a1b83600d2d33e00e5bd906735503
shubhammg98 <[shubhammg510@gmail.com])>
2023-11-11 15:04:17 +0530

    checkout

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -1,16 +1,16 @@
- def call(String project, String ImageTag, String shubhamdevops){
-     withCredentials([usernamePassword(
-             credentialsId: 'shubhamdevops',
-             passwordVariable: 'PASS',
-             usernameVariable: 'USER'
+ //def call(String project, String ImageTag, String shubhamdevops){
+    // withCredentials([usernamePassword(
+    //         credentialsId: 'shubhamdevops',
+     //        passwordVariable: 'PASS',
+      //       usernameVariable: 'USER'
 
              
-     )]) {
-         sh "docker login -u '$USER' -p '$PASS'"
-     }
-     sh "docker image push ${shubhamdevops}/${project}:${ImageTag}"
-     sh "docker image push ${shubhamdevops}/${project}:latest"   
- }
+    // )]) {
+     //    sh "docker login -u '$USER' -p '$PASS'"
+    // }
+    // sh "docker image push ${shubhamdevops}/${project}:${ImageTag}"
+    // sh "docker image push ${shubhamdevops}/${project}:latest"   
+ //}
 //withCredentials([usernamePassword(
   //  credentialsId: 'shubhamdevops',
     // passwordVariable: 'PASS',
